cd3335073ab82f0f2c56ee2ae1a930880e6526e3
Raj <raj.shahrma@gmail.com>
2026-07-08 23:32:05 +0530

    crew

---
+++ Enterprise-CrewAI/app/test_crew.py
@@ -0,0 +1,15 @@
+from app.crew.crew import crew
+
+result = crew.kickoff(
+
+    inputs={
+
+        "question":
+
+        "Show me monthly sales from January to June"
+
+    }
+
+)
+
+print(result)
